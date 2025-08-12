49dca11a8f1ec0e738810237fefeebfaa9ae8409
martynapatyniak <martynapatyniak41@gmail.com>
2025-08-12 12:03:35 +0200

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,22 +1,35 @@
 module.exports = function (eleventyConfig) {
   // kopiowanie plików statycznych
-  eleventyConfig.addPassthroughCopy("styles.css");
-  eleventyConfig.addPassthroughCopy("base_icon_white_background.png");
-  eleventyConfig.addPassthroughCopy("base_logo_white_background.png");
-  eleventyConfig.addPassthroughCopy("logo.png");
-
+  eleventyConfig.addPassthroughCopy("src/styles.css");
+  eleventyConfig.addPassthroughCopy("src/base_icon_white_background.png");
+  eleventyConfig.addPassthroughCopy("src/base_logo_white_background.png");
+  eleventyConfig.addPassthroughCopy("src/logo.png");
+  
   // alias layoutu
   eleventyConfig.addLayoutAlias("layout", "layout.njk");
-
-  // kolekcja postów z folderu content/blog
+  
+  // kolekcja postów - poprawiona ścieżka
   eleventyConfig.addCollection("posts", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("content/blog/*.md").reverse();
+    return collectionApi.getFilteredByGlob("src/content/blog/*.md").reverse();
+  });
+
+  // filtry dla dat
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return new Date(dateObj).toLocaleDateString('pl-PL', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    });
+  });
+
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return new Date(dateObj).toISOString().split('T')[0];
   });
 
   return {
     dir: {
-      input: "src",         // katalog z treścią
-      includes: "_includes",// katalog z szablonami
+      input: "src",
+      includes: "_includes",
       output: "_site"
     }
   };
